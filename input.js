872e804871e60ce786040327a2bf3cf21017c9dc
Aaron Tenn <aaron.will.tenn@gmail.com>
2021-09-22 16:18:19 -0400

    Added commands to movement

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -7,9 +10,22 @@ const setupInput = function () {
   return stdin;
 };
 
+
 const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
+  }
+  else if (key === 'w') {
+    connection.write("Move: up")
+  }
+  else if (key === 'a') {
+    connection.write("Move: left")
+  }
+  else if (key === 's') {
+    connection.write("Move: down")
+  }
+  else if (key === 'd') {
+    connection.write("Move: right")
   }
 };
 
